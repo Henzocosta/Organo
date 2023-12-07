98a509589d12f25fe95cdea7fe79e6c2292c52c0
Henzocosta <henzocosta10@gmail.com>
2023-12-07 17:20:58 -0300

    Atualizacao de Projeto

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -1,13 +1,20 @@
 // Lembrando que componentes reacts comecam com letra maiuscula
+import { useState } from 'react'
 import './campoTexto.css'
 
 const CampoTexto = (props) => {
     
     const placeholderModificada = `${props.placeholder}`
+
+   
+
+    const aoDigitar = (evento) => {
+        props.aoAlterado(evento.target.value)
+    }
     return (
         <div className="campo-texto">
             <label>{props.label}</label>
-            <input placeholder= {placeholderModificada}/>
+            <input value = {props.valor} onChange = {aoDigitar} required = {props.obrigatorio} placeholder= {placeholderModificada}/>
         </div>
     )
 }
